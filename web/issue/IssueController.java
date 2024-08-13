1bf88608ee475081bb8bec6c991650ee2d6174fc
manmanmeer <masak6161@gmail.com>
2024-08-13 21:34:23 +0900

    作成処理追加

--- web/issue/IssueController.java
+++ web/issue/IssueController.java
@@ -6,20 +6,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
 
         model.addAttribute("issueList", issueService.findAll());
-        return "/issues/List";
+        return "issues/List";
     }
 
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute IssueForm form) {
+        return "issues/creationForm";
+    }
+
+    @PostMapping
+    public  String create(IssueForm form, Model model) {
+        return showList(model);
+    }
 }
---
+++ web/issue/IssueForm.java
@@ -0,0 +1,11 @@
+package com.example.ITS.web.issue;
+
+import lombok.Data;
+
+@Data
+public class IssueForm {
+
+        private String summary;
+        private String description;
+
+}
