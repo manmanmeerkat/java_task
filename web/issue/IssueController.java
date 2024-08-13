e2ff9bb200c58726102ed520774939f95f57a228
manmanmeer <masak6161@gmail.com>
2024-08-13 16:14:43 +0900

    second commit

--- web/issue/IssueController.java
+++ web/issue/IssueController.java
@@ -2,6 +2,7 @@ package com.example.ITS.web.issue;
 
 import com.example.ITS.domain.Issue.IssueEntity;
 import com.example.ITS.domain.Issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     @GetMapping("/issues")
     public String showList(Model model) {
