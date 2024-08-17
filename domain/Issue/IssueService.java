ceabff744a810d08631bf3d1d6dd9eff50c6a3b2
manmanmeer <masak6161@gmail.com>
2024-08-17 21:12:30 +0900

    insert

--- domain/Issue/IssueService.java
+++ domain/Issue/IssueService.java
@@ -14,4 +14,8 @@ public class IssueService {
     public List<IssueEntity> findAll() {
       return issueRepository.findAll();
     }
+
+    public void create(String summary, String description) {
+        issueRepository.insert(summary, description);
+    }
 }
--- web/issue/IssueController.java
+++ web/issue/IssueController.java
@@ -33,6 +33,7 @@ public class IssueController {
 
     @PostMapping
     public  String create(IssueForm form, Model model) {
+        issueService.create(form.getSummary(), form.getDescription());
         return showList(model);
     }
 }
